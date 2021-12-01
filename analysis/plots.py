02a82a0395ab9b8407d6bca275160d7d579db9cc
YuXie <richard9596@gmail.com>
2021-11-30 23:31:42 -0500

    add code to run experiments on cluster

---
+++ analysis/plots.py
@@ -0,0 +1,67 @@
+import os.path as osp
+
+import matplotlib.pyplot as plt
+
+from configs.config_global import FIG_DIR
+
+plt.rcParams.update({'font.size': 16})
+line_styles = ['-', '--', ':']
+# Default colors
+# colors = ['red', 'tomato', 'green', 'lightgreen', 'blue', 'lightblue']
+
+
+# Plot testing loss recorded during training
+def plot_train_log_loss(data, exp_name):
+    plt.figure()
+    legends = []
+    for i_, datum in enumerate(data):
+        model_n, data_f = datum
+        x_axis = data_f['DataNum']
+        trainloss = data_f['TrainLoss']
+        testloss = data_f['TestLoss']
+        plt.plot(x_axis, trainloss, color='C'+str(i_), linestyle='--')
+        plt.plot(x_axis, testloss, color='C'+str(i_))
+        legends += [model_n + " Train Loss"]
+        legends += [model_n + " Test Loss"]
+
+    plt.legend(legends, loc='best', fontsize=6)
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    # plt.xlim([0, 4e4])
+    plt.ylim([0, 3])
+    plt.xlabel('Train Data')
+    plt.ylabel('Loss')
+
+    ax = plt.gca()
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    plt.tight_layout(pad=0.5)
+    plt.savefig(osp.join(FIG_DIR, exp_name + 'train_log_loss.pdf'))
+
+
+# Plot testing accuracy recorded during training
+def plot_train_log_acc(data, exp_name):
+    plt.figure()
+    legends = []
+    for i_, datum in enumerate(data):
+        model_n, data_f = datum
+        x_axis = data_f['DataNum']
+        testacc = data_f['TestAcc']
+        plt.plot(x_axis, testacc, color='C'+str(i_))
+        legends += [model_n + " Test Acc"]
+
+    plt.legend(legends, loc='best', fontsize=6)
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    # plt.xlim([0, 4e4])
+    plt.ylim([0, 100])
+    plt.xlabel('Train Data')
+    plt.ylabel('Accuracy')
+
+    ax = plt.gca()
+    # Hide the right and top spines
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    plt.tight_layout(pad=0.5)
+    plt.savefig(osp.join(FIG_DIR, exp_name + 'train_log_acc.pdf'))
---
+++ configs/experiments.py
@@ -0,0 +1,45 @@
+"""Experiments and corresponding analysis.
+format adapted from https://github.com/gyyang/olfaction_evolution
+
+Each experiment is described by a function that returns a list of configurations
+function name is the experiment name
+
+combinatorial mode:
+    config_ranges should not have repetitive values
+sequential mode:
+    config_ranges values should have equal length,
+    otherwise this will only loop through the shortest one
+control mode:
+    base_config must contain keys in config_ranges
+"""
+import os
+from collections import OrderedDict
+import logging
+
+from configs.config_global import ROOT_DIR, LOG_LEVEL
+from configs.configs import BaseConfig
+from utils.config_utils import vary_config
+
+from analysis.train_analysis import plot_train_log
+
+
+def init_analysis(configs_):
+    logging.basicConfig(level=LOG_LEVEL)
+    exp_name = configs_[0].experiment_name
+    print('Analyzing ' + exp_name)
+    exp_path = os.path.join(ROOT_DIR, 'experiments', exp_name) + os.sep
+    plot_train_log([exp_path], exp_name=exp_name)
+
+
+# -----------------------------------------------------
+# experiments
+# -----------------------------------------------------
+
+
+def test():
+    config = BaseConfig()
+    config.experiment_name = 'test'
+
+    config_ranges = OrderedDict()
+    configs = vary_config(config, config_ranges, mode='combinatorial')
+    return configs
